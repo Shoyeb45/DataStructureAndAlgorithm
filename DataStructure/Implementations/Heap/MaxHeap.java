55da1cb3bbd44be36b7cbd4e605c6371b6f3abce
Shoyeb45 <shoyebff45@gmail.com>
2024-07-20 09:28:45 +0530

    Implemented Max Heap in Java

--- DataStructure/Implementations/Heap/MaxHeap.java
+++ DataStructure/Implementations/Heap/MaxHeap.java
@@ -1,18 +1,24 @@
-
 class MaxHeap {
     public static final int MAX_SIZE = 100000;
 
     private int sz = 0;
     private int[] storedData = new int[MAX_SIZE];
 
-    private static void swap(int[] array, int index1, int index2) {
+    private void swap(int[] array, int index1, int index2) {
         int temp = array[index1];
         array[index1] = array[index2];
         array[index2] = temp;
     }
-    void push(int data) throws MaxSizeReachedException {
+
+    /**
+     * Method for inserting value in heap.
+     * @param data
+     * @throws MaxSizeReachedException
+     */
+    void push(int data) {
         if(sz == MAX_SIZE) {
-            throw new MaxSizeReachedException("Size limit reached");
+            System.err.println("Maximum capacity of heap reached.");
+            return;
         }
         int idx = sz;
         storedData[idx] = data;
@@ -29,39 +35,68 @@ class MaxHeap {
     }
 
     /**
-     * Function for checking if heap is empty or not
-     * @return true or false
+     * Method for checking if heap is empty or not
+     * @return true if it is empty else false
      */
     boolean empty() {
         return this.size() == 0;
     }
 
+    /**
+     * Method for getting the size of heap
+     * @return size in int
+     */
     int size() {
         return sz;
     }
 
+    /**
+     * @return Maximum element or top element 
+     */ 
     int top() {
         if(this.empty()) {
-            throw new EmptyDataStructureException("Heap is empty\n");
+            System.err.println("Heap is empty.");
+            return -1;
         }
         return storedData[0];
     }
-
+    
+    /**
+     * Private method for calculating parent index
+     * @param x
+     * @return parent index
+     */
     private int parent(int x) {
         return (x - 1) / 2;
     }
+    
+    /**
+     * Private method for calculating left child index
+     * @param x
+     * @return left child index
+     */
     private int leftChild(int x) {
         return 2 * x + 1;
     }
+    /**
+     * Private method for calculating right child index
+     * @param x
+     * @return right child index
+     */
     private int rightChild(int x) {
         return 2 * x + 2;
     }
-
+    
+    /**
+     * Method for deleting top element or maximum element in heap
+     */
     void pop() {
         if(sz == 0) {
-            
+            System.err.println("Heap is empty.");
+            return;
         }
         storedData[0] = storedData[sz - 1];
+        storedData[sz - 1] = 0;
         sz--;
         int idx = 0;
         while(true) {
@@ -72,14 +107,28 @@ class MaxHeap {
             int maximum = Math.max(currElement, Math.max(leftChildElement, rightChildElement));
             if(maximum == currElement) {
                 return;
-            } else if(maximum > leftChildElement) {
-                swap(storedData, leftChild(idx), maximum);
+            } else if(maximum == leftChildElement) {
+                swap(storedData, leftChild(idx), idx);
                 idx = leftChild(idx);
-            } else if(maximum > rightChildElement) {
-                swap(storedData, rightChild(idx), maximum);
+            } else if(maximum == rightChildElement) {
+                swap(storedData, rightChild(idx), idx);
                 idx = rightChild(idx);
             }
         }
     }
 
+    public String toString() {
+        StringBuilder str = new StringBuilder("");
+        for(int i = 0; i < sz; i++) {
+            if(i == 0) {
+                str.append("[" + Integer.toString(storedData[i]) + ",");
+            } else if(i == sz - 1) {
+                str.append(" " + Integer.toString(storedData[i]) + "]");
+            } else {
+                str.append(" " + Integer.toString(storedData[i]) + ",");
+            }
+        }
+        return str.toString();
+    }
+    
 }
--- DataStructure/Implementations/Heap/TestingImple.java
+++ DataStructure/Implementations/Heap/TestingImple.java
@@ -1,5 +1,30 @@
 public class TestingImple {
     public static void main(String[] args) {
+        MaxHeap mxheap = new MaxHeap();
+        mxheap.push(10);
+        mxheap.push(12);
+        System.out.println(mxheap.top());
+        System.out.println(mxheap);
+        mxheap.push(5);
+        System.out.println(mxheap);
+        mxheap.push(34);
+        System.out.println(mxheap);
+        System.out.println((!mxheap.empty() ? "Size: " + mxheap.size(): "Heap is empty"));
         
+        
+        mxheap.push(76);
+        System.out.println(mxheap);
+        mxheap.push(20);
+        System.out.println(mxheap);
+        
+        mxheap.pop();
+        System.out.println(mxheap);
+
+        mxheap.pop();
+        System.out.println(mxheap);
+        
+        mxheap.pop();
+        System.out.println(mxheap);
+
     }
 }
