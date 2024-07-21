afd51b110289383b669bc2aa03e2293950e3e9e7
Shoyeb45 <shoyebff45@gmail.com>
2024-07-21 19:27:09 +0530

    Detect cycle - using DFS

--- Algorithms/Graph/DetectCycleInGraph.cpp
+++ Algorithms/Graph/DetectCycleInGraph.cpp
@@ -7,6 +7,16 @@ typedef long long ll;
 const ll mod = 1000000007;
 
 // BFS Method to detect Node
+bool isCycleBFS(int V, vector<int> adj[]) {
+    vector<bool> visited(V);
+    loop(i, V) {
+        if(!visited[i])
+            if(bfs(i, adj, visited))
+                return true;
+    }
+    return false;
+}
+
 bool bfs(int curr, vector<int> adj[], vector<bool> &visited) {
     queue<pair<int, int>> q;
     visited[curr] = 1;
@@ -27,12 +37,32 @@ bool bfs(int curr, vector<int> adj[], vector<bool> &visited) {
     return false;
 }
 
-bool isCycle(int V, vector<int> adj[]) {
-    vector<bool> visited(V);
-    loop(i, V) {
-        if(bfs(i, adj, visited))
-            return true;
+
+
+// DFS Method
+bool isCycleDFS(int V, vector<int> adj[]) {
+    vector<bool> visited(V, 0);
+    for(int i = 0; i < V; i++) {
+        if(!visited[i]) {
+            if(dfs(i, -1, adj, visited))
+                return true;
+        }
     }
+    return false;
+}
+
+bool dfs(int currNode, int parent, vector<int> adj[], vector<bool> &visited) {
+    visited[currNode] = 1;
+
+    for(auto adjNode: adj[currNode]) {
+        if(!visited[adjNode]) {
+            if(dfs(adjNode, currNode, adj, visited))
+                return true;
+        } else if(adjNode != parent) {
+            return true;
+        }
+    }
+    
     return false;
 }
 
@@ -48,6 +78,6 @@ int main() {
         adjacencyList[y].push_back(x);
     }
 
-    cout << (isCycle(nodes, adjacencyList)? "Yes!! There is cylce\n": "No!! There is no cycle\n");
+    cout << (isCycleBFS(nodes, adjacencyList)? "Yes!! There is cylce\n": "No!! There is no cycle\n");
     return 0;
 }
