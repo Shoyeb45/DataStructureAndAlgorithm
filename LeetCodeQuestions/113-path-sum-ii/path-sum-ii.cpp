c174b673fac29e91a3ac6385e9bda791a2542908
Shoyeb Ansari <shoyebff45@gmail.com>
2025-01-09 15:28:08 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (74.94%) - LeetSync

--- LeetCodeQuestions/113-path-sum-ii/path-sum-ii.cpp
+++ LeetCodeQuestions/113-path-sum-ii/path-sum-ii.cpp
@@ -12,29 +12,27 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void allPath(TreeNode* root, vector<int> path, int sum, int targetSum) {
+    void allPath(TreeNode* root, vector<int> &path, int sum, int targetSum) {
         if (!root) {
-            return;
-        }
-        if (!root -> left && !root -> right) {
-            sum += root -> val;
-            path.push_back(root -> val);
-            if (sum == targetSum) {
-                ans.push_back(path);
-            }
             return;
         }
         sum += root -> val;
         path.push_back(root -> val);
-        allPath(root -> left, path, sum, targetSum); 
-        allPath(root -> right, path, sum, targetSum); 
+
+        if (!root -> left && !root -> right && sum == targetSum) {
+            ans.push_back(path);
+        } else {
+            allPath(root -> left, path, sum, targetSum); 
+            allPath(root -> right, path, sum, targetSum); 
+        }
+        path.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         if (!root) {
             return {};
         }
-        
-        allPath(root, {}, 0, targetSum);
+        vector<int> temp;
+        allPath(root, temp, 0, targetSum);
         return ans;
     }
 };
