e2685c6dc40a5f27ff6c01bbaced170397d74f16
Shoyeb Ansari <shoyebff45@gmail.com>
2025-01-17 11:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (25.48%) - LeetSync

---
+++ LeetCodeQuestions/669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* f(TreeNode* root, int low, int high) {
+        if (!root) {
+            return nullptr;
+        }
+        if (root -> val < low) {
+            return f(root -> right, low, high);
+        }
+        else if (root -> val > high) {
+            return f(root -> left, low, high);
+        }
+        else {
+            root -> left = f(root -> left, low, high);
+            root -> right = f(root -> right, low, high);
+        }
+        return root;
+    }
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if (!root) {
+            return root;
+        }
+        TreeNode* temp = root;
+        return f(temp, low, high);
+    }
+};
