ce24bf5a04617d51fdd34a49375e6847e570dbae
Shoyeb Ansari <shoyebff45@gmail.com>
2025-01-25 16:00:49 +0530

    Time: 12 ms (45.74%) | Memory: 65.1 MB (41.04%) - LeetSync

---
+++ LeetCodeQuestions/287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int low = 1, high = n - 1;
+        int ans = -1;
+
+        std::function<bool(int)> check = [&](int mid) {
+            int num_smaller = 0;
+            for (auto x : nums) {
+                num_smaller += (x <= mid);
+            }
+
+            // By piegon hole principle
+            if (num_smaller > mid) {
+                return true;
+            }
+            return false;
+        };
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (check(mid)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
