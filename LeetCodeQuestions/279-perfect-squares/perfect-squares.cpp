9210fe45fd72e78415b459af6a94f650dd21f968
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-03 20:11:13 +0530

    Time: 87 ms (28.65%) | Memory: 38.5 MB (13.59%) - LeetSync

--- LeetCodeQuestions/279-perfect-squares/perfect-squares.cpp
+++ LeetCodeQuestions/279-perfect-squares/perfect-squares.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<int> dp;
-
     Solution() {
         dp.resize((int)(1e4 + 1), -1);
     }
-
-    int numSquares(int n, int oper = 0) {
-        if (n == 0) {
-            return oper;
+    int numSquares(int n) {
+        if (n <= 3) {
+            return n;
         }
 
         if (dp[n] != -1) {
-            return dp[n] + oper; 
+            return dp[n];
         }
 
         int ans = INT_MAX;
-        
         for (int i = 1; i * i <= n; i++) {
-            ans = min(ans, numSquares(n - i * i, oper + 1));
+            ans = min(ans, numSquares(n - i * i) + 1);
         }
 
-        dp[n] = ans - oper;
-        return ans;
+        return dp[n] = ans;
     }
 };
