06ec394f6a2935989314a16d87ab00c2ca9fed48
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-07 19:35:05 +0530

    Time: 3 ms (31.34%) | Memory: 16.5 MB (14.40%) - LeetSync

--- LeetCodeQuestions/64-minimum-path-sum/minimum-path-sum.cpp
+++ LeetCodeQuestions/64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
-    int dp[201][201];
-
 public:
-    Solution() {
-        memset(dp, -1, sizeof(dp));
-    }
-    int minPathSum(vector<vector<int>>& a, int row = 0, int col = 0) {
-        if (row >= a.size() || col >= a[0].size()) {
-            return INT_MAX;
-        }
-        if (dp[row][col] != -1) {
-            return dp[row][col];
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int a = INT_MAX, b = INT_MAX;
+                if (i - 1 >= 0) {
+                    a = dp[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    b = dp[i][j - 1];
+                }
+
+                dp[i][j] = grid[i][j] + min(a, b);
+            }
         }
 
-        if (row == a.size() - 1 && col == a[0].size() - 1) {
-            return a[row][col];
-        }
-
-        int right = minPathSum(a, row, col + 1);
-        int down = minPathSum(a, row + 1, col);
-
-        return dp[row][col] = min(right, down) + a[row][col];
+        return dp[n - 1][m - 1];
     }
 };
