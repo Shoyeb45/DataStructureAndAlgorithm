cc0ba47be705e61bee608d31e869cb42fd220ff7
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-08 10:06:03 +0530

    Time: 9 ms (86.98%) | Memory: 85 MB (14.13%) - LeetSync

--- LeetCodeQuestions/1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
+++ LeetCodeQuestions/1394-minimum-path-cost-in-a-grid/minimum-path-cost-in-a-grid.cpp
@@ -2,35 +2,25 @@
 
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int f(vector<vector<int>> &grid, vector<vector<int>> &moveCost, int col, int row = 0) {
-        if (row == grid.size() - 1) {
-            return grid[row][col];
-        }
-
-        if (dp[row][col] != -1) {
-            return dp[row][col];
-        }
-
-        int ans = INT_MAX;
-        for (int i = 0; i < grid[0].size(); i++) {
-            ans = min(ans, f(grid, moveCost, i, row + 1) + grid[row][col] + moveCost[grid[row][col]][i]) ;
-        }
-
-        return dp[row][col] = ans;
-    }
-
     int minPathCost(vector<vector<int>>& grid, vector<vector<int>>& moveCost) {
         NFS
-        int ans = INT_MAX;
-        dp.resize(grid.size() , vector<int>(grid[0].size(), -1));
+        int n = grid.size(), m = grid[0].size();
 
-        for (int i = 0; i < grid[0].size(); i++) {
-            int d = f(grid, moveCost, i);
-            cout << d << "\n";
-            ans = min(ans, d);
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+
+        dp[0] = grid[0];
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = grid[i][j]; 
+                int temp = INT_MAX;
+                for (int k = 0; k < m; k++) {
+                    temp = min(temp, dp[i - 1][k] + moveCost[grid[i - 1][k]][j]);
+                }
+                dp[i][j] += temp; 
+            }
         }
-        return ans;
+
+        return *min_element(dp.back().begin(), dp.back().end());
     }
 };
