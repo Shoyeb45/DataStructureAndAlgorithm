b9eac5d42678ce9f4cbd609828217a9f7bd5606d
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-09 22:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 77 MB (62.60%) - LeetSync

---
+++ LeetCodeQuestions/1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& a) {
+        int ans = a[0];
+
+        for (int i = 1; i < a.size(); i++) {
+            if (a[i] - a[i - 1] > 0) {
+                ans += (a[i] - a[i - 1]);
+            }
+        }
+
+        return ans;
+    }
+};
