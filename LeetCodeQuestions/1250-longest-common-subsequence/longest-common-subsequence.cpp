84c89722f42775fd1d0e2887eced6c94ba942c25
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-17 12:01:23 +0530

    Time: 48 ms (8.88%) | Memory: 27.3 MB (75.03%) - LeetSync

--- LeetCodeQuestions/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ LeetCodeQuestions/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,35 +1,38 @@
-// Recursive dp
+// Iterative Dp
+
 class Solution {
 public:
-    int dp[1001][1001];
+    int longestCommonSubsequence(string s1, string s2) {
+        // Define dp table
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n, vector<int> (m, 0));
 
-    int f(string &s1, string &s2, int idx1 = 0, int idx2 = 0) {
-        if (idx1 >= s1.size() || idx2 >= s2.size()) {
-            return 0;
-        }
+        // expression : if equal : dp[i][j] = 1 + dp[i - 1][j - 1], else : dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-        if (dp[idx1][idx2] != -1) {
-            return dp[idx1][idx2];
-        }
+        // Building dp table
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int a = 0, b = 0, c = 0;
 
-        int ans = 0;
-        if (s1[idx1] == s2[idx2]) {
-            ans = 1 + f(s1, s2, idx1 + 1, idx2 + 1);
-        }
-        else {
-            int incr1 = f(s1, s2, idx1 + 1, idx2);
-            int incr2 = f(s1, s2, idx1, idx2 + 1);
-            ans = max({incr1, incr2});
-        }
-        return dp[idx1][idx2] = ans;
-    }
+                if (i - 1 >= 0) {
+                    a = dp[i - 1][j]; 
+                }
+                if (j - 1 >= 0) {
+                    b = dp[i][j - 1];
+                }
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    c = dp[i - 1][j - 1];
+                }
 
-    int longestCommonSubsequence(string text1, string text2) {
-        // start from 0, 0
-        // if s1[i] == s2[j], then add and go to next problem, i.e., i + 1, j + 1
-        // if not then incremenet i and j by one in turn
-        // Then we'll have maximum answer
-        memset(dp, -1, sizeof(dp));
-        return f(text1, text2);    
+                if (s1[i] == s2[j]) {
+                    dp[i][j] = 1 + c;
+                }
+                else {
+                    dp[i][j] = max(a, b);
+                }
+            }
+        } 
+
+        return dp[n - 1][m - 1];
     }
 };
