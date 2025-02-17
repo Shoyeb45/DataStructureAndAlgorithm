1965fa645947b2f303c460277e2e04cb90c49a0a
Shoyeb Ansari <shoyebff45@gmail.com>
2025-02-17 11:32:46 +0530

    Time: 75 ms (5.22%) | Memory: 14.2 MB (79.45%) - LeetSync

--- LeetCodeQuestions/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ LeetCodeQuestions/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,3 +1,4 @@
+// Recursive dp
 class Solution {
 public:
     int dp[1001][1001];
@@ -18,13 +19,16 @@ public:
         else {
             int incr1 = f(s1, s2, idx1 + 1, idx2);
             int incr2 = f(s1, s2, idx1, idx2 + 1);
-            // int incrBoth = f(s1, s2, idx1 + 1, idx2 + 1);
             ans = max({incr1, incr2});
         }
         return dp[idx1][idx2] = ans;
     }
 
     int longestCommonSubsequence(string text1, string text2) {
+        // start from 0, 0
+        // if s1[i] == s2[j], then add and go to next problem, i.e., i + 1, j + 1
+        // if not then incremenet i and j by one in turn
+        // Then we'll have maximum answer
         memset(dp, -1, sizeof(dp));
         return f(text1, text2);    
     }
