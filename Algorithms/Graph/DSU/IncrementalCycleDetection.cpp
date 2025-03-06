7c66b883a08b6c67e12d0a6553ee5352ec4d223f
Shoyeb45 <shoyebff45@gmail.com>
2025-03-06 11:14:14 +0530

    Incremental Cycle Detection

---
+++ Algorithms/Graph/DSU/IncrementalCycleDetection.cpp
@@ -0,0 +1,18 @@
+/*
+ Problem Description:
+ Processing a series of queries that add undirected edges to a graph and determining after each addition whether the graph contains a cycle.
+ If there is cycle don't process them else add the edge in the graph.
+*/ 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int q;
+    cin >> q;
+
+    for (int i = 0, u, v; i < q; i++) {
+        cin >> u >> v;
+        
+    }
+}
