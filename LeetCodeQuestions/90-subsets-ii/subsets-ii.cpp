48a31256975ddcd6ff38a8a8940d0393fc1b971e
Shoyeb Ansari <shoyebff45@gmail.com>
2025-03-22 12:43:28 +0530

    Time: 2 ms (45.60%) | Memory: 13.3 MB (20.96%) - LeetSync

--- LeetCodeQuestions/90-subsets-ii/subsets-ii.cpp
+++ LeetCodeQuestions/90-subsets-ii/subsets-ii.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    void genUniqueSubsets(map<int, int> &mp, std::map<int, int>::iterator it, vector<int> &a, vector<vector<int>> &ans) {
-        if (it == mp.end()) {
+    void f(int i, vector<int> &nums, vector<int> &a, vector<vector<int>> &ans) {
+        if (i >= nums.size()) {
             ans.push_back(a);
             return;
         }
 
-        int val = it -> first;
-        int freq = it -> second;
-        genUniqueSubsets(mp, next(it), a, ans);
+        int cnt = 0;
+        int ele = nums[i];
+        while (i < nums.size() && nums[i] == ele) {
+            i++, cnt++;
+        } 
 
-        for (int i = 0; i < freq; i++) {
-            a.push_back(val);
-            genUniqueSubsets(mp, next(it), a, ans);
+        // Take zero times
+        f(i, nums, a, ans);
+
+        for (int j = 0; j < cnt; j++) {
+            a.push_back(ele);
+            f(i, nums, a, ans);
         }
 
-        for (int i = 0; i < freq; i++) {
+        // remove all the elements
+        for (int i = 0; i < cnt; i++) {
             a.pop_back();
         }
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> a;
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        map<int, int> mp;
-        for (auto x : nums) {
-            mp[x]++;
-        }
-
-        genUniqueSubsets(mp, mp.begin(), a, ans);
-        return ans;
+        vector<int> a;
+        f(0, nums, a, ans);
+        return ans;        
     }
 };
