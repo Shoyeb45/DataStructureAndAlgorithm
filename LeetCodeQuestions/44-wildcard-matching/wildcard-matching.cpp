bb5c1142882b3d344ab864602cdacdd5bcccc7f5
Shoyeb Ansari <shoyebff45@gmail.com>
2025-03-24 06:22:37 +0530

    Time: 1394 ms (5.01%) | Memory: 27.6 MB (40.09%) - LeetSync

--- LeetCodeQuestions/44-wildcard-matching/wildcard-matching.cpp
+++ LeetCodeQuestions/44-wildcard-matching/wildcard-matching.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int dp[2001][2001];
-
+    vector<int> prefStar;
     string s, p;
     int f(int i, int j) {
         if (i >= s.size() && j >= p.size()) {
@@ -11,19 +11,13 @@ public:
             return 0;
         }
         else if (i >= s.size()) {
-            while (j < p.size()) {
-                if (p[j] != '*') {
-                    return 0;
-                }
-                j++;
-            }
-            return 1;
+            return (prefStar[(int)p.size()] - prefStar[j]) == (int)p.size() - j;
         }
 
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        
+
         if (p[j] == '*') {
             return dp[i][j] = f(i, j + 1) || f(i + 1, j);
         }
@@ -39,6 +33,16 @@ public:
         memset(dp, -1, sizeof(dp));
 
         this -> s = s, this -> p = p;
+
+        prefStar.assign(p.size() + 1, 0);
+
+        for (int i = 1; i <= p.size(); i++) {
+            prefStar[i] = prefStar[i - 1] + (p[i - 1] == '*');
+        } 
+        for (int i = 1; i <= p.size(); i++) {
+            cout << prefStar[i] << " ";
+        }
+        cout <<"\n";
         return f(0, 0);
     }
 };
