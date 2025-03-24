d7785d741855ed25b6c775dfcc24580062422472
Shoyeb Ansari <shoyebff45@gmail.com>
2025-03-24 05:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (50.28%) - LeetSync

--- LeetCodeQuestions/10-regular-expression-matching/regular-expression-matching.cpp
+++ LeetCodeQuestions/10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 string s, p;
+int dp[21][21];
 public:
     int f(int i, int j) {
         if (i >= s.size() && j >= p.size()) {
@@ -9,17 +10,23 @@ public:
             return 0;
         }
 
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        
         bool match = (i < s.size() && (s[i] == p[j] || p[j] == '.'));
 
         if (j + 1 < p.size() && p[j + 1] == '*') {
-            return f(i, j + 2) || (match? f(i + 1, j): false);
+            return dp[i][j] = f(i, j + 2) || (match? f(i + 1, j): false);
         } 
         else if (match) {
-            return f(i + 1, j + 1);
+            return dp[i][j] = f(i + 1, j + 1);
         }
         return 0;
     }
+
     bool isMatch(string s, string p) {
+        memset(dp, -1, sizeof(dp));
         this -> s = s, this -> p = p;
         return f(0, 0);
     }
