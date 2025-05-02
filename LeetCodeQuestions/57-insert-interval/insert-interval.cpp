f452bac7106252989fd306ad38ac2919936b00c3
Shoyeb Ansari <shoyebff45@gmail.com>
2025-05-03 00:08:13 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (45.89%) - LeetSync

--- LeetCodeQuestions/57-insert-interval/insert-interval.cpp
+++ LeetCodeQuestions/57-insert-interval/insert-interval.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
-    bool is_mergeable(vector<int> &i1, vector<int> &i2){
-        return !(i1[1] < i2[0] || i2[1] < i1[0]);
-    }
 
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> new_interval = {intervals[0]};
-
-        int n = intervals.size();
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> new_intervals;
 
         auto is_mergeable = [&](vector<int> &i1, vector<int> &i2) -> bool {
             return !(i1[1] < i2[0] || i2[1] < i1[0]);
         };
+        int n = intervals.size();
 
-        for (int i = 1; i < n; i++) {
-            if (is_mergeable(intervals[i], new_interval.back())) {
-                vector<int> n = {
-                    min(intervals[i][0], new_interval.back()[0]),
-                    max(intervals[i][1], new_interval.back()[1]),
+        int i = 0; 
+
+        for (; i < n; i++) {
+            if (is_mergeable(newInterval, intervals[i])) {
+                newInterval = {
+                    min(newInterval[0], intervals[i][0]),
+                    max(newInterval[1], intervals[i][1]),
                 };
-                new_interval.pop_back();
-                new_interval.push_back(n);
-            }   
-            else {
-                new_interval.push_back(intervals[i]);
-            } 
-        }
-        return new_interval;
-    }
-
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>> new_intervals;
-
-        int i;
-        for (i = 0; i < intervals.size(); i++) {
-            if (intervals[i][0] >= newInterval[0]) {
-                break;
             }
-        }
-        intervals.insert(intervals.begin() + i, newInterval);
+            else if (intervals[i][0] < newInterval[0]) {
+                new_intervals.push_back(intervals[i]);
+            }
+            else {
+                new_intervals.push_back(newInterval);
+                while (i < n) {
+                    new_intervals.push_back(intervals[i++]);
+                }
+                return new_intervals;
+            }
+        }    
 
-        return merge(intervals);
+
+        new_intervals.push_back(newInterval);
+
+        return new_intervals;
     }
 };
