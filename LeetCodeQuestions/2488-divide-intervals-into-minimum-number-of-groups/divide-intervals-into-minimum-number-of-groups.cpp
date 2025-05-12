7c8899b96fd35f31ad8e777944a3438a22fd097a
Shoyeb Ansari <shoyebff45@gmail.com>
2025-05-12 13:24:12 +0530

    Time: 82 ms (60.92%) | Memory: 92.9 MB (94.75%) - LeetSync

--- LeetCodeQuestions/2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
+++ LeetCodeQuestions/2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        int freq[(int)(1e6 + 2)];
-        memset(freq, 0, sizeof freq);
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
 
-        for (auto x : intervals) {
-            freq[x[0]]++;
-            cout << x[1] + 1 << "\n"; 
-            freq[x[1] + 1]--;
-        }
+        sort(intervals.begin(), intervals.end());
+        // pq.push(intervals[0][1]);
 
-        for (int i = 1; i <= (int)1e6 + 1; i++) {
-            freq[i] += freq[i - 1]; 
+        for (int i = 0; i < intervals.size(); i++) {
+            if (!pq.empty() && intervals[i][0] > pq.top()) {
+                pq.pop();
+            } 
+            
+            pq.push(intervals[i][1]);
+
         }
-        return *max_element(freq, freq + (int)1e6 + 2);
+        return pq.size();
     }
 };
