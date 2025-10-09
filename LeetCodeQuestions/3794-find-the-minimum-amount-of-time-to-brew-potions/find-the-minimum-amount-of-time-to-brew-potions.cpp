86e07b0f033bf518bc911ad0eac25fbbbabea02e
Shoyeb Ansari <shoyebff45@gmail.com>
2025-10-09 22:51:41 +0530

    Time: 81 ms (98.99%) | Memory: 44.4 MB (97.47%) - LeetSync

---
+++ LeetCodeQuestions/3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -0,0 +1,23 @@
+using ll = long long;
+
+class Solution {
+public:
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        int n = skill.size(), m = mana.size();
+        partial_sum(skill.begin(), skill.end(), skill.begin());
+
+        ll skill0 = skill[0];
+        ll start = 0;
+        for (int i = 1; i < m; i++) {
+            ll prev = mana[i - 1], curr = mana[i];
+
+            ll mx = 1LL * skill0 * prev;
+            for (int j = 1; j < n; j++) {
+                mx = max(mx, 1LL * skill[j] * prev- 1LL * skill[j - 1] * curr);
+            }
+            start += mx;
+        }   
+
+        return start + 1LL * skill.back() * mana[m - 1];
+    }
+};
