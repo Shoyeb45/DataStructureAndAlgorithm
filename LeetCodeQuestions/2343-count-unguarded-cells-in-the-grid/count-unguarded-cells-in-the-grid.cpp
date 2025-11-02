ccafb92606d8fb7af0b66afeadca0d46893446a5
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-02 21:54:21 +0530

    Time: 143 ms (37.62%) | Memory: 195 MB (34.86%) - LeetSync

--- LeetCodeQuestions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ LeetCodeQuestions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         swap(n, m);
+
         // 1 -> Guard, 2 -> Wall
         vector<vector<int>> mat(n, vector<int>(m, 0));
         for (auto g: guards) {
@@ -10,45 +11,25 @@ public:
         for (auto w: walls) {
             mat[w[0]][w[1]] = 2;
         }
-        queue<pair<pair<int, int>, pair<int, int>>> q;
-        vector<vector<bool>> vis(n, vector<bool> (m, false));
-
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, -1, 1};
-        for (auto g: guards) {
-            mat[g[0]][g[1]] = 2;
+
+
+        for (auto& g: guards) {
+            int x = g[0], y = g[1];
             for (int k = 0; k < 4; k++) {
-                int nx = g[0] + dx[k], ny = g[1] + dy[k];
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                    continue;
+                int nx = x + dx[k], ny = y + dy[k];
+                while (nx >= 0 && ny >=0 && nx < n && ny < m && mat[nx][ny] != 2 && mat[nx][ny] != 1 && mat[nx][ny] != 2) {
+                    mat[nx][ny] = 3;
+                    nx += dx[k], ny += dy[k];
                 }
-                q.push(make_pair(make_pair(g[0], g[1]), make_pair(dx[k], dy[k])));
             }
         }
 
-
-        while (!q.empty()) {
-            int x = q.front().first.first, y = q.front().first.second;
-            int d_x = q.front().second.first, d_y = q.front().second.second;
-            q.pop();
-
-            int nx = x + d_x, ny = y + d_y;
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
-                continue;
-            }
-            if (mat[nx][ny] == 2) {
-                continue;
-            }
-        
-            mat[nx][ny] = 1;
-            q.push(make_pair(make_pair(nx, ny), make_pair(d_x, d_y)));
-            vis[nx][ny] = true;
-        }
+    
         int ans = 0;
-        for (auto x: mat) {
-            for (auto y: x) {
-                ans += (y == 0);
-            }
+        for (auto &x: mat) {
+            ans += count(x.begin(), x.end(), 0);
         }
         return ans;
     }
