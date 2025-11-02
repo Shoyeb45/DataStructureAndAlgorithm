648ed0d59cdebd4925331c0b4af8667ee99ad53a
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-02 21:57:06 +0530

    Time: 153 ms (34.40%) | Memory: 189.7 MB (38.99%) - LeetSync

--- LeetCodeQuestions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
+++ LeetCodeQuestions/2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.cpp
@@ -4,12 +4,12 @@ public:
         swap(n, m);
 
         // 1 -> Guard, 2 -> Wall
-        vector<vector<int>> mat(n, vector<int>(m, 0));
+        vector<vector<char>> mat(n, vector<char>(m, '0'));
         for (auto g: guards) {
-            mat[g[0]][g[1]] = 1;
+            mat[g[0]][g[1]] = '1';
         }
         for (auto w: walls) {
-            mat[w[0]][w[1]] = 2;
+            mat[w[0]][w[1]] = '2';
         }
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, -1, 1};
@@ -19,8 +19,8 @@ public:
             int x = g[0], y = g[1];
             for (int k = 0; k < 4; k++) {
                 int nx = x + dx[k], ny = y + dy[k];
-                while (nx >= 0 && ny >=0 && nx < n && ny < m && mat[nx][ny] != 2 && mat[nx][ny] != 1 && mat[nx][ny] != 2) {
-                    mat[nx][ny] = 3;
+                while (nx >= 0 && ny >=0 && nx < n && ny < m && mat[nx][ny] != '2' && mat[nx][ny] != '1' && mat[nx][ny] != '2') {
+                    mat[nx][ny] = '3';
                     nx += dx[k], ny += dy[k];
                 }
             }
@@ -29,7 +29,7 @@ public:
     
         int ans = 0;
         for (auto &x: mat) {
-            ans += count(x.begin(), x.end(), 0);
+            ans += count(x.begin(), x.end(), '0');
         }
         return ans;
     }
