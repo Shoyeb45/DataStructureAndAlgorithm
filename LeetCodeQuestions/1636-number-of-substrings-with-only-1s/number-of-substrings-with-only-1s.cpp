4a19c785423ae2ab09407c7de6429c54ab186d94
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-16 11:52:55 +0530

    Time: 88 ms (5.14%) | Memory: 11.7 MB (39.33%) - LeetSync

---
+++ LeetCodeQuestions/1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,51 @@
+const int MOD = (int) 1e9 + 7;
+using ll = long long;
+
+class Solution {
+public:
+    // a^b % mod
+    int mod_pow(int base, int b, int mod) {
+        ll ans = 1;
+        ll a = base;
+
+        while (b > 0) {
+            if ((b & 1)) {
+                ans = (ll)ans * a % mod;
+            }
+            a = (ll)a * a % mod;
+            b >>= 1;
+        }
+        return (int) ans;
+    }
+
+    
+
+    int numSub(string s) {
+        ll ans = 0;
+        int n = s.size();
+
+        int mod_inverse_2 = mod_pow(2, MOD - 2, MOD) % MOD;
+
+        for (int i = 0; i < n; ) {
+            if (s[i] == '0') {
+                i++;
+                continue;
+            }
+
+            int j = i;
+            while (j < n && s[j] == s[i]) {
+                j++;
+            }
+            int cnt = j - i;
+            i = j;
+            // cout << cnt << "\n";
+
+            ll sum = (ll)cnt * (cnt + 1) % MOD;
+            sum = (ll)sum * mod_inverse_2 % MOD;
+
+            cout << sum << "\n";
+            ans = ((ll) (ans + sum) % MOD); 
+        }
+        return (int) ans;
+    }
+};
