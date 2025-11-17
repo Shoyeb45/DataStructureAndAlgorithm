3ef54a1c802ed9c2eaa4038e4bdfd787c6181f57
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-17 14:53:58 +0530

    Time: 3 ms (15.64%) | Memory: 61.4 MB (74.91%) - LeetSync

--- LeetCodeQuestions/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
+++ LeetCodeQuestions/1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool kLengthApart(vector<int>& nums, int k) {
-        vector<int> idxs;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == 1) {
-                idxs.push_back(i);
-            }
-        }
         
-        for (int i = 0; i < (int) idxs.size() - 1; i++) {
-            cout << idxs[i] << " " << idxs[i + 1] << "\n";
-            if (abs(-idxs[i] + idxs[i + 1] - 1) < k) {
+        int prev = -1;
+        
+        for (int i = 0; i < (int) nums.size(); i++) {
+            if (nums[i] == 1 && prev != -1 && i - prev - 1 < k) {
                 return false;
             }
+            if (nums[i] == 1) {
+                prev = i;
+            }
         }
+
         return true;
     }
 };
