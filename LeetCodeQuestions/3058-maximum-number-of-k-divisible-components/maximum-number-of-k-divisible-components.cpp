2afe76658b0bdc337f52fee70ef1a145be7e4577
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-28 10:27:54 +0530

    Time: 167 ms (28.11%) | Memory: 179.2 MB (49.11%) - LeetSync

--- LeetCodeQuestions/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ LeetCodeQuestions/3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -8,28 +8,23 @@ public:
             adj[ed[1]].push_back(ed[0]);
         }   
 
-        for (auto& x: values) x %= k;
-
         int ans = 0;
+
         std::function<int(int, int)> dfs = [&](int u, int parent) -> int {
-            int overall_sum = values[u];
+            int overall_sum = values[u] % k;
 
             for (auto &v: adj[u]) {
                 if (v != parent) {
-                    int temp = dfs(v, u);
-
-                    if (temp != -1) {
-                        overall_sum = (overall_sum + temp) % k;
-                    }
+                    overall_sum = (overall_sum + dfs(v, u)) % k;
+                    overall_sum %= k;
                 }
             }
-            overall_sum %= k;
-            if (overall_sum == 0) {
-                ans++;
-                overall_sum = -1;
-            }
+
+            ans += (overall_sum == 0);
+
             return overall_sum;
         };
+
         dfs(0, -1);
 
         return ans;
