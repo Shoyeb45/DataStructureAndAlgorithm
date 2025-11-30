4b5bf8fa87a05699de7948e4a6d3e3b31585cc03
Shoyeb Ansari <shoyebff45@gmail.com>
2025-11-30 12:50:48 +0530

    Time: 906 ms (5.29%) | Memory: 95 MB (7.82%) - LeetSync

---
+++ LeetCodeQuestions/1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minSubarray(vector<int>& a, int p) {
+        int sum = 0;
+        for (auto& x: a) {
+            sum = (sum + x) % p;
+            x %= p;    
+        }
+
+        if (sum == 0) return 0;
+
+        // need to remove sum + k * p, where k >= 0
+        // Need to find the mininum length subarray for above
+        // sum[l:r] = sum + (p * k)
+        // pref_r - pref_{l-1} = sum + (p * k)
+        // pref_r - (sum + p * k) = pref_{l-1}
+        // pr - pl = sum
+        // pr - sum = pl
+        int n = a.size();
+
+        vector<long long> pref(n + 1);
+
+        for (int i = 1; i <= n; i++) pref[i] = (pref[i - 1] + a[i - 1]);
+        int ans = INT_MAX;
+        for (int i = 1; i <= n; i++) {
+            cout << pref[i] << " ";
+        }
+
+        map<long long, int> mp;
+        mp[0] = 0;
+        // 3 4 8 10
+        for (int i = 1; i <= n; i++) {
+            long long temp = (pref[i] - sum + p) % p;
+            while (temp >= 0) {
+                if (mp.find(temp % p) != mp.end()) {
+                    ans = min(ans, i - mp[temp % p]);
+                } else break;
+                temp -= p;
+            }
+            mp[pref[i] % p] = i;
+        }
+
+        return ans == n || ans == INT_MAX ? -1: ans;
+    }
+};
