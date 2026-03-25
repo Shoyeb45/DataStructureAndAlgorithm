b9081fc77274d59bbd43ff7452d89938a0da16f2
Shoyeb Ansari <shoyebff45@gmail.com>
2026-03-25 09:08:45 +0530

    Time: 74 ms (5.04%) | Memory: 158.5 MB (6.84%) - LeetSync

--- LeetCodeQuestions/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ LeetCodeQuestions/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -5,46 +5,28 @@ public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<ll> row(n), col(m);
+        vector<vector<ll>> pref(n + 1, vector<ll> (m + 1, 0));
 
-        for (int i = 0; i < n; i++) {
-            row[i] = accumulate(grid[i].begin(), grid[i].end(), 0LL);
+        for (int i = 1; i <= n; i++) {
+            pref[i][1] = pref[i - 1][1] + grid[i - 1][0];
         }
 
-        for (int j = 0; j < m; j++) {
-            col[j] = 0;
-            for (int i = 0; i < n; i++) col[j] += grid[i][j];
+        for (int j = 1; j <= m; j++) {
+            pref[1][j] = pref[1][j - 1] + grid[0][j - 1];
         }
 
-        auto prefix_find = [&](vector<ll> &a) -> vector<ll> {
-            vector<ll> pref(a.begin(), a.end());
-            for (int i = 1; i < a.size(); i++) pref[i] += pref[i - 1];
-            return pref;
-        };
-        auto suffix_find = [&](vector<ll> &a) -> vector<ll> {
-            vector<ll> suff(a.begin(), a.end());
-            for (int i = (int) a.size() - 2; i >= 0; i--) suff[i] += suff[i + 1];
-            return suff;
-        };
-
-        auto pref_row = prefix_find(row);
-        auto pref_col = prefix_find(col);
-        
-        auto suff_row = suffix_find(row);
-        auto suff_col = suffix_find(col);
-
-        // 3   2  1  4
-        // 3   5  6  10
-        // 10  7  5  4
-        auto check_ans = [&](vector<ll> &pref, vector<ll> &suff) -> bool {
-            int n = pref.size();
-            int sum = 0;
-            for (int i = 0; i < n - 1; i++) {
-                if (pref[i] == suff[i + 1]) return true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                pref[i][j] = pref[i - 1][j] + pref[i][j - 1] + grid[i - 1][j - 1] - pref[i - 1][j - 1];
             }
-            return false;
-        };
+        }
 
-        return check_ans(pref_row, suff_row) || check_ans(pref_col, suff_col);
+        for (int i = 1; i <= n; i++) {
+            if (pref[i][m] == pref[n][m] - pref[i][m]) return true;
+        }
+        for (int j = 1; j <= m; j++) {
+            if (pref[n][j] == pref[n][m] - pref[n][j]) return true;
+        }
+        return false;
     }
 };
