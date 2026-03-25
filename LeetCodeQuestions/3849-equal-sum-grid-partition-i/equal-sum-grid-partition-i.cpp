2ddd6a4ab8b3dcb18c7cabb6b9a3bfb5a5a3c5c4
Shoyeb Ansari <shoyebff45@gmail.com>
2026-03-25 09:11:51 +0530

    Time: 8 ms (66.55%) | Memory: 130 MB (60.43%) - LeetSync

--- LeetCodeQuestions/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
+++ LeetCodeQuestions/3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.cpp
@@ -5,28 +5,32 @@ public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
 
-        vector<vector<ll>> pref(n + 1, vector<ll> (m + 1, 0));
+        vector<ll> row(n), col(m);
 
-        for (int i = 1; i <= n; i++) {
-            pref[i][1] = pref[i - 1][1] + grid[i - 1][0];
+        for (int i = 0; i < n; i++) {
+            row[i] = accumulate(grid[i].begin(), grid[i].end(), 0LL);
         }
 
-        for (int j = 1; j <= m; j++) {
-            pref[1][j] = pref[1][j - 1] + grid[0][j - 1];
+        for (int j = 0; j < m; j++) {
+            col[j] = 0;
+            for (int i = 0; i < n; i++) col[j] += grid[i][j];
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                pref[i][j] = pref[i - 1][j] + pref[i][j - 1] + grid[i - 1][j - 1] - pref[i - 1][j - 1];
+        auto prefix_find = [&](vector<ll> &a) -> void {
+            for (int i = 1; i < a.size(); i++) a[i] += a[i - 1];
+        };
+     
+        prefix_find(row);
+        prefix_find(col);
+ 
+        auto check_ans = [&](vector<ll> &a) -> bool {
+            int n = a.size();
+            for (int i = 0; i < n - 1; i++) {
+                if (2 * a[i] == a[n - 1]) return true;
             }
-        }
+            return false;
+        };
 
-        for (int i = 1; i <= n; i++) {
-            if (pref[i][m] == pref[n][m] - pref[i][m]) return true;
-        }
-        for (int j = 1; j <= m; j++) {
-            if (pref[n][j] == pref[n][m] - pref[n][j]) return true;
-        }
-        return false;
+        return check_ans(row) || check_ans(col);
     }
 };
