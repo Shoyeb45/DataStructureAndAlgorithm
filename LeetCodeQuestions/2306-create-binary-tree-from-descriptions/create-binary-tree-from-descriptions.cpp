07017ce39cd22bbaa25343fe8364e6d5aeff7e70
Shoyeb Ansari <shoyebff45@gmail.com>
2026-06-07 10:30:41 +0530

    Time: 294 ms (19.51%) | Memory: 279.5 MB (38.18%) - LeetSync

---
+++ LeetCodeQuestions/2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* createBinaryTree(vector<vector<int>>& desc) {
+        map<int, pair<int, int>> mp;
+
+        for (auto &x: desc) {
+            if (mp.find(x[0]) != mp.end()) {
+                if (x[2]) {
+                    mp[x[0]].first = x[1];
+                } else {
+                    mp[x[0]].second = x[1];
+                }
+            } else {
+                if (x[2]) {
+                    mp[x[0]] = make_pair(x[1], -1);
+                } else {
+                    mp[x[0]] = make_pair(-1, x[1]);
+                }
+            }
+        }
+        
+        set<int> st;
+        for (auto x: mp) {
+            st.insert(x.second.first);
+            st.insert(x.second.second);
+        }
+
+        int rootVal = -1;
+        for (auto x: mp) {
+            if (!st.count(x.first)) {
+                rootVal = x.first;
+                break;
+            }
+        }
+        
+        TreeNode *root = new TreeNode(rootVal);
+        
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+            if (mp.find(node->val) == mp.end()) continue;
+
+            auto p = mp[node->val];
+            if (p.first != -1) {
+                node->left = new TreeNode(p.first);
+                q.push(node->left);
+            }
+            if (p.second != -1) {
+                node->right = new TreeNode(p.second);
+                q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
